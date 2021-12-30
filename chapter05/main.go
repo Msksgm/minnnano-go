8014b57095ba14a729e217bac6200491921b1d7c
Msksgm <msksgm3124@gmail.com>
2021-12-31 08:34:20 +0900

    fix: nullを削除

--- chapter05/main.go
+++ chapter05/main.go
@@ -23,4 +23,20 @@ func main() {
 	fmt.Printf("xv = %d\n", xv.Int())
 	xv.SetInt(100)
 	fmt.Printf("xv (after SetInt) = %d\n", xv.Int())
+
+	rv1 := reflect.ValueOf(1)
+	// rv2 := reflect.ValueOf("Hello, World")// panic
+	// rv3 := reflect.ValueOf([]byte{0xa, 0xd}) // panic
+	// rv4 := reflect.ValueOf(make(chan struct{})) // panic
+
+	fmt.Println(rv1.Int())
+	// fmt.Println(rv2.Int()) //panic
+	// fmt.Println(rv3.Int()) //panic
+	// fmt.Println(rv4.Int()) //panic
+
+	rv = reflect.ValueOf(map[string]int{"foo": 1})
+	value := rv.MapIndex(reflect.ValueOf("foo"))
+	fmt.Println(value)
+	rv.SetMapIndex(reflect.ValueOf("foo"), reflect.ValueOf(2))
+	// rv.Int() // panic
 }
