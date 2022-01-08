68417ea1693a825d4f2c80297917fe2c78e6b66e
Msksgm <msksgm3124@gmail.com>
2022-01-08 16:32:05 +0900

    add: p.149

---
+++ chapter07/a1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
